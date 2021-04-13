05deb01fe9da8fe370cdbfd8563902706a8a71b3
abhishekbarve0612 <barveabhishek.ab@gmail.com>
2021-04-14 00:10:43 +0530

    Completed Assignment

---
+++ src/components/CityDropdown.js
@@ -0,0 +1,17 @@
+import React from "react";
+const CityDropdown = ({ states, selectedState }) => {
+  const cityList = states[0].city;
+  console.log(typeof cityList);
+  return (
+    <div>
+      <label htmlFor="cityList">Select City</label>
+      <select name="cityList" id="cityList">
+        {cityList.map((city, key) => (
+          <option value={key}>{city.name}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default CityDropdown;
---
+++ src/components/StateDropdown.js
@@ -0,0 +1,25 @@
+import React from "react";
+const StateDropdown = ({ states, changeState }) => {
+  return (
+    <div>
+      <label htmlFor="state">States:</label>
+      <select
+        className="form-control"
+        name="stateList"
+        id="state"
+        onChange={(e) => changeState(e)}
+      >
+        {" "}
+        {states.map((state, key) => {
+          return (
+            <option value={key} key={key}>
+              {state.name}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default StateDropdown;
